1393d7708786bbc2ed754147407e2edbaf3ca7e0
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-18 11:14:01 -0300

    feat(components): create ProductCard component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import { Card, CardContent, CardTitle } from "./ui/card";
+import { Product } from "@/types/product";
+
+type Props = {
+  product: Product;
+};
+
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card>
+      <img
+        src={product.imageUrl}
+        alt={product.name}
+        className="w-full h-48 object-cover rounded-t-xl"
+      />
+      <CardContent>
+        <CardTitle className="text-lg">{product.name}</CardTitle>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
+        <p className="font-bold mt-2">R$ {product.price.toFixed(2)}</p>
+      </CardContent>
+    </Card>
+  );
+}
