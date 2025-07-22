cfb4e4a079b2a239f36850daa7b791d7b830ff8c
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-22 20:18:06 -0300

    feat: add initial product list with sample data for catalog

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -6,20 +6,45 @@ export const products: Product[] = [
     name: "Cafeteira Elétrica",
     description: "Faz café fresquinho em minutos.",
     price: 199.99,
-    imageUrl: "https://source.unsplash.com/400x400/?coffee",
+    imageUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_867428-MLU75990739657_042024-O.webp",
   },
   {
     id: "2",
     name: "Liquidificador",
     description: "Ideal para sucos e vitaminas.",
     price: 149.9,
-    imageUrl: "https://source.unsplash.com/400x400/?blender",
+    imageUrl: "https://imgs.casasbahia.com.br/11439321/1xg.jpg?imwidth=500",
   },
   {
     id: "3",
     name: "Air Fryer",
     description: "Frita sem óleo, mais saudável!",
     price: 399.0,
-    imageUrl: "https://source.unsplash.com/400x400/?airfryer",
+    imageUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_630787-MLA88041710016_072025-O.webp",
+  },
+  {
+    id: "4",
+    name: "Cafeteira Elétrica",
+    description: "Faz café fresquinho em minutos.",
+    price: 199.99,
+    imageUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_867428-MLU75990739657_042024-O.webp",
+  },
+  {
+    id: "5",
+    name: "Liquidificador",
+    description: "Ideal para sucos e vitaminas.",
+    price: 149.9,
+    imageUrl: "https://imgs.casasbahia.com.br/11439321/1xg.jpg?imwidth=500",
+  },
+  {
+    id: "6",
+    name: "Air Fryer",
+    description: "Frita sem óleo, mais saudável!",
+    price: 399.0,
+    imageUrl:
+      "https://http2.mlstatic.com/D_NQ_NP_630787-MLA88041710016_072025-O.webp",
   },
 ];
