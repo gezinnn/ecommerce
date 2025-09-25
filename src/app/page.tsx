6f1c613a685aab79966b307a40ad3136514e1225
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-09-25 17:27:33 -0300

    feat: add Header component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Carousel from "@/components/Carousel";
+import Header from "@/components/Header";
 import ProductCard from "@/components/ProductCard";
 import { products } from "@/lib/products";
 
 export default function Home() {
   return (
     <>
+    <Header/>
       <Carousel />
 
+  
       <main className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
